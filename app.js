dc306dd20a515f96cb8f85fb3e13c5d7d474b540
Harvey Spectre <aloha.jeet@gmail.com>
2021-06-18 21:26:36 +0530

    ready to try deploying

---
+++ app.js
@@ -0,0 +1,111 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+const MongoDBStore = require('connect-mongo')(session);
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,'public')));
+
+const secret = process.env.SECRET||'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24*60*60
+});
+
+store.on("error", function(e){
+    console.log('Session Store Error', e)
+})
+
+const sessionConfig = {
+    store,
+    secret,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        httpOnly:true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge:1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+app.get('/fakeUser', async(req,res) => {
+    const user = new User({email: 'colttt@gmail.com', username:'colttt'});
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
+})
+
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
+
+app.get('/', (req,res) => {
+    res.render('home');
+})
+
+app.all('*',(req,res,next) => {
+    next(new ExpressError('Page Not Found',404));
+})
+
+app.use((err,req,res,next) => {
+    const {status = 500} = err;
+    if(!err.message) err.message = 'The error did not had a message';
+    res.render('error',{err});
+})
+
+app.listen(3000, () => {
+    console.log("Server started listening.");
+})
