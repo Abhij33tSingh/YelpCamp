358ec63a2edad5f8e8a1fb1ee606e9e706f9bb8d
Harvey Spectre <aloha.jeet@gmail.com>
2021-06-18 21:35:56 +0530

    added start script

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ app.use((err,req,res,next) => {
     res.render('error',{err});
 })
 
-app.listen(3000, () => {
-    console.log("Server started listening.");
+const port = process.env.PORT||3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}.`);
 })
